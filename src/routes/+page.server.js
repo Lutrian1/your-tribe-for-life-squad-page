5162d4a1f1200d355c77f8d3222631b7d00f4bc2
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-05 12:13:43 +0200

    FIX: 505 ERROR
    Er bevonden zich 2 <script> tags in de pages, dit heb ik even snel aangepast

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,19 +1,3 @@
-export async function load ({ url }) {
-
-    //hier komt het fetchen van data check oude server.js met alles
-
-    // return {members: membersData.data} // Return hier dus de data (Dit is geen render)
-}
-
-
-
-
-
-
-
-
-
-
 export async function load({ url }) {
   //hier komt het fetchen van data check oude server.js met alles
   const membersResponse = await fetch(
