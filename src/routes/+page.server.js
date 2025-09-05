a3dea3d5964782879e0b715633449833dcab7b39
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-05 12:05:02 +0200

    Poging 4 tot het fixen van de code conflict

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,9 @@
 export async function load ({ url }) {
 
-  //hier komt het fetchen van data check oude server.js met alles
+    //hier komt het fetchen van data check oude server.js met alles
 
+    // return {members: membersData.data} // Return hier dus de data (Dit is geen render)
 }
-
 
 
 
