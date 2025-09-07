5180b486f94d66f4fafa9ca7663eab2e7a73f6f8
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-07 21:19:35 +0200

    FEAT: Landing Page - Bekijk De Klassen section aangemaakt
    Description: Ik heb de section aangemaakt. Verder heb ik ook een aantal korte aanpassing gemaakt met CLAMP, omdat de EM te groot werd. Ik heb nu px gebruikt. Ik ga nu animeren

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import logo from '$lib/assets/pictures/logo.svg';
 	import stylesheet from '$lib/css/stylesheet.css';
-	import scribble from '$lib/assets/pictures/scribble.svg';
 
 	let { children } = $props();
 </script>
